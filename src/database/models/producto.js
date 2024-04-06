47b4b4ff8273ed3220ddd7ef06bce600db439c2c
Caro Valverde <carolina.valverde255@gmail.com>
2024-04-06 19:20:55 -0300

    agrego nombre, precio, imagen

--- src/database/models/producto.js
+++ src/database/models/producto.js
@@ -1,7 +1,31 @@
 import mongoose, { Schema } from "mongoose";
 
 const productoSchema = new Schema({
-  
+  nombreProducto: {
+    type: String,
+    required: true,
+    minLength: 4,
+    maxLength: 50,
+    unique: true,
+  },
+  precio: {
+    type: Number,
+    required: true,
+    min: 100, //prueba para ver si funciona con valor negativo, si no lo ponemos si dejaria
+    max: 10000,
+  },
+  imagen: {
+    type: String,
+    requiered: true,
+    validate: {
+      validator: (dato) => {
+        const pattern = /^(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png)$/;
+        return pattern.test(dato);
+      },
+    },
+  },
+ 
+});
 //aqui creamos un modelo
 const Producto = mongoose.model('producto', productoSchema); //ponerlo siempre en singular xq adiciona una s para crear la coleccion
 
