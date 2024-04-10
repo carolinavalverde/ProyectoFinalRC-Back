9f7c250b93a1119929545fa243e7473bc161e018
Figueroa Fernando <feroa88@gmail.com>
2024-04-10 16:32:26 -0300

    elimino comentarios del codigo

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,17 +1,16 @@
 import { validationResult } from "express-validator";
 import Producto from "../database/models/producto.js";
 
-export const listarProductos = async(req, res) => {
+export const listarProductos = async (req, res) => {
   try {
-   //pedir a la bd la lista de todos los productos
-   const productos = await Producto.find()
-   //responder al frontend con el array de productos
-   res.status(200).json(productos)
+    const productos = await Producto.find();
+
+    res.status(200).json(productos);
   } catch (err) {
-   console.error(err);
-   res.status(400).json({mensaje:'Error al buscar los productos'})
-  }
- };
+    console.error(err);
+    res.status(400).json({ mensaje: "Error al buscar los productos" });
+  }
+};
 
 export const obtenerProducto = async (req, res) => {
   try {
@@ -47,21 +46,19 @@ export const editarProducto = async (req, res) => {
 
 export const borrarProducto = async (req, res) => {
   try {
-      //verificar si el producto existe con el id correspondiente
-      const productoBuscado = await Producto.findById(req.params.id);
-      //si no existe contestar con un status 404
-      if (!productoBuscado) {
-          return res
-          .status(404)
-          .json({ mensaje: "El id enviado no corresponde a ningun producto" });
-      }
-      await Producto.findByIdAndDelete(req.params.id);
-      res
-          .status(200)
-          .json({ mensaje: "El producto fue eliminado correctamente" });
-      } catch (err) {
-      console.error(err);
-      res.status(500).json({ mensaje: "Error al borrar el producto" });
-      }
-  };
-  
+    const productoBuscado = await Producto.findById(req.params.id);
+
+    if (!productoBuscado) {
+      return res
+        .status(404)
+        .json({ mensaje: "El id enviado no corresponde a ningun producto" });
+    }
+    await Producto.findByIdAndDelete(req.params.id);
+    res
+      .status(200)
+      .json({ mensaje: "El producto fue eliminado correctamente" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ mensaje: "Error al borrar el producto" });
+  }
+};
