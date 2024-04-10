136cf50deacf6f15d2b8a00046d5339324da74be
Bruno Cordoba <brunonahuel87@gmail.com>
2024-04-10 18:52:54 -0300

    enviar una respuesta cuando puedo crear efectivamente el producto 201

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -69,6 +69,10 @@ export const crearProducto = async(req, res)=>{
   try {
 
     const productoNuevo = new Producto(req.body);
+    
+     await productoNuevo.save();
+    
+     res.status(201).json({mensaje: 'El producto fue creado correctamente'})
    
   } catch (error) {
    
