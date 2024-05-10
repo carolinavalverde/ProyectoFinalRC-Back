a8d3dcf25e280162ecb871bffaefd5601e749ccf
Caro Valverde <carolina.valverde255@gmail.com>
2024-05-10 16:51:34 -0300

    agrego mas configuraciones de cors

--- index.js
+++ index.js
@@ -9,14 +9,28 @@ import { fileURLToPath } from "url";
 import "./src/database/database.js";
 
 const app = express();
+
 app.set("port", process.env.PORT || 4000);
+
 app.listen(app.get("port"), () => {
   console.log("Estoy en el puerto " + app.get("port"));
 });
 
+// Configurar CORS con opciones específicas
 app.use(
   cors({
-    origin: "https://prueba-restaurant.netlify.app",
+    origin: function (origin, callback) {
+      // Permitir peticiones de cualquier origen, pero solo si provienen de localhost o de la URL de producción
+      if (
+        !origin ||
+        origin === "http://localhost:5173" ||
+        origin === "https://prueba-restaurant.netlify.app"
+      ) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
