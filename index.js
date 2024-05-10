268b3063dcd397d4bba5069976a8f88aa0bf4ec2
Caro Valverde <carolina.valverde255@gmail.com>
2024-05-10 17:19:36 -0300

    agrego mas configuraciones de cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import "dotenv/config";
+import cors from "cors";
 import morgan from "morgan";
 import productosRouter from "./src/routes/productos.routes.js";
 import usuarioRouter from "./src/routes/usuario.routes.js";
@@ -9,30 +10,27 @@ import "./src/database/database.js";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "https://prueba-restaurant.netlify.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.set("port", process.env.PORT || 4000);
 
 app.listen(app.get("port"), () => {
   console.log("Estoy en el puerto " + app.get("port"));
 });
 
-// Middleware para manejar los encabezados CORS
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://prueba-restaurant.netlify.app"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
-
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "/public")));
 
 app.use("/api", productosRouter);
